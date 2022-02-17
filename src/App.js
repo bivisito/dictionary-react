83110e43b7efa0ddf42c03c0ee8f3af2f736bd79
bivisito <92517765+bivisito@users.noreply.github.com>
2022-02-17 19:57:59 +0200

    Added the search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      Hello World
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Victoria </small>
+      </footer>
     </div>
   );
 }
