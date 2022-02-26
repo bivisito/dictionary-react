70ff0c33b6ac60dcd794fac8d5dc4750ce1596b9
bivisito <92517765+bivisito@users.noreply.github.com>
2022-02-26 22:37:08 +0200

    Added photos API and styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyWord);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     // documentation: https://api.dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001cd0235f570eb44c48f6d8b84c17a6fda";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -36,7 +47,7 @@ export default function Dictionary(props) {
   if (loaded) {
     return (
       <div className="Dictionary">
-        <h1 class="text-center">Dictionary App</h1>
+        <h1 className="text-center">Dictionary App</h1>
         <section>
           <h2>What word do you want to look up?</h2>
           <form onSubmit={handleSubmit}>
@@ -50,6 +61,7 @@ export default function Dictionary(props) {
           <div className="hint">i.e. yoga, book, happiness, beach</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
