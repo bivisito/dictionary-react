ff8914d5ad3d1d17288a0c624c331ee1f66d615d
bivisito <92517765+bivisito@users.noreply.github.com>
2022-02-26 22:47:33 +0200

    Updated the Photos.js alt again x2

--- src/Photos.js
+++ src/Photos.js
@@ -12,7 +12,7 @@ export default function Photos(props) {
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.tiny}
-                    alt="image of word searched"
+                    alt={photo.src.photographer}
                     className="img-fluid"
                   />
                 </a>
