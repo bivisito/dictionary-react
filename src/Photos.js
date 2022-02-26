9006122aafcf043276843bdbd3309bbe365fa1ba
bivisito <92517765+bivisito@users.noreply.github.com>
2022-02-26 22:43:49 +0200

    Updated Photos.js again

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,11 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.tiny} alt="photo" className="img-fluid" />
+                  <img
+                    src={photo.src.tiny}
+                    alt="image of word searched"
+                    className="img-fluid"
+                  />
                 </a>
               </div>
             );
