1bf0fee6965bec0743b6714bac7ffb0765c0672e
bivisito <92517765+bivisito@users.noreply.github.com>
2022-03-01 00:02:48 +0200

    Added the new background image and more styling

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <main>
-        <Dictionary defaultKeyWord="sunset" />
-      </main>
-      <footer className="App-footer">
-        <small>Coded by Victoria T </small>
-      </footer>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary defaultKeyWord="sunset" />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Victoria T </small>
+        </footer>
+      </div>
     </div>
   );
 }
